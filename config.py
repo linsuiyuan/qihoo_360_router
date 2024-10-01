594ec1de601d18b766a0b0a4c0d8896a8bbb84be
linsuiyuan <linsuiyuan@icloud.com>
2024-10-02 00:23:52 +0800

    修改环境变量的读取方式

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
 """配置文件"""
 import json
 import os
+from urllib import parse
 
 # 360路由用户名密码
-USERNAME = os.getenv('USERNAME')
+QIHOO_USER = os.getenv('QIHOO_USER')
+QIHOO_USER = parse.parse_qs(QIHOO_USER)
+
+USERNAME = QIHOO_USER['USERNAME'][0]
 assert USERNAME is not None
-PASSWORD = os.getenv('PASSWORD')
+PASSWORD = QIHOO_USER['PASSWORD'][0]
 assert PASSWORD is not None
 
 
-SPEEDLIMIT_LIST = os.getenv('SPEEDLIMIT_LIST')
+SPEEDLIMIT_LIST = os.getenv('QIHOO_SPEEDLIMIT_LIST')
 assert SPEEDLIMIT_LIST is not None
 SPEEDLIMIT_LIST = json.loads(SPEEDLIMIT_LIST)
 for d in SPEEDLIMIT_LIST:
