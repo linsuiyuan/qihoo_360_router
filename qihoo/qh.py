34b67967df7b453ce40e76f74d684d32fc381503
Willow Lin <willowlin@WillowdeMac-mini.local>
2024-11-17 15:51:07 +0800

    重构，完成 qh 模块的 SpeedLimit、Devices、VirtualService 类

--- qihoo/qh.py
+++ qihoo/qh.py
@@ -11,10 +11,16 @@ class Router:
     req: httpx.Client
 
 
-class BlackList:
+class RouterPlugin:
+    """路由插件，实现路由某项功能的类的基类"""
 
     def __init__(self, router: Router):
+        self.router = router
         self.req = router.req
+
+
+class BlackList(RouterPlugin):
+    """黑名单类"""
 
     def list(self):
         """获取黑名单列表"""
@@ -61,6 +67,124 @@ class BlackList:
         return response.json()
 
 
+class SpeedLimit(RouterPlugin):
+    """
+    360路由限制设备网络速度。
+    (貌似没有连接的也可以设置，路由有保存，但限速的设置看不到)
+    """
+
+    def _set_speed_limit(self, enable, mac, upload, download):
+        data = {
+            'enable': f'{enable}',
+            'mac': mac,
+            'upload': f'{upload}',
+            'download': f'{download}',
+        }
+
+        response = self.req.post(f'{ROUTE_URL}/app/devices/webs/setspeedlimit.cgi',
+                                 data=data)
+        response.raise_for_status()
+        return response.json()
+
+    def set_speed_limit(self, mac, upload, download):
+        """设置设备限速"""
+        return self._set_speed_limit(enable=1, mac=mac, upload=upload, download=download)
+
+    def cancel_speed_limit(self, mac):
+        """取消限速"""
+        return self._set_speed_limit(enable=0, mac=mac, upload=0, download=0)
+
+
+class Devices(RouterPlugin):
+    """360路由设备管理Mixin类，黑名单管理，限速管理等"""
+
+    def topology_info(self):
+        """获取拓扑网络所有设备列表信息"""
+        response = self.req.get(f'{ROUTE_URL}/router/mesh_get_topology_info.cgi')
+        response.raise_for_status()
+
+        # client_node 表示设备节点
+        # mesh_node 表示拓扑路由节点
+        route_node = response.json()['data'][0]
+
+        return route_node
+
+    def list(self):
+        """获取连接的设备列表"""
+        response = self.req.get(f'{ROUTE_URL}/app/devices/webs/getdeviceslist.cgi')
+        response.raise_for_status()
+        data = response.json()['data']
+        return data
+
+
+class VirtualService(RouterPlugin):
+    """
+    端口映射相关功能
+    """
+
+    def list(self):
+        """端口映射列表"""
+        response = self.req.post(f'{ROUTE_URL}/app/portmap/webs/virtual_service_list_show.cgi')
+        response.raise_for_status()
+        return response.json()
+
+    def _add_del(self, name, internal_ip, external_port, internal_port, mode):
+        """
+        添加或删除端口映射
+        :param name: 端口名称
+        :param internal_ip: 内部ip
+        :param external_port: 外部端口
+        :param internal_port: 内部端口
+        :param mode: 添加或删除，0表示添加，2表示删除
+        """
+        data = {
+            'name': name,
+            'in_ip': internal_ip,
+            'protocol': 'tcp',
+            'out_start_port': external_port,
+            'out_end_port': external_port,
+            'in_start_port': internal_port,
+            'in_end_port': internal_port,
+            'uiname': 'ALL',
+            'mode': mode,
+        }
+
+        response = self.req.post(f'{ROUTE_URL}/app/portmap/webs/virtual_service_add_del.cgi',
+                                 data=data)
+        response.raise_for_status()
+
+        return response.json()
+
+    def add(self, name, internal_ip, external_port, internal_port):
+        """
+        添加端口映射
+        """
+        return self._add_del(name=name,
+                             internal_ip=internal_ip,
+                             external_port=external_port,
+                             internal_port=internal_port,
+                             mode=0)
+
+    def delete(self, name, internal_ip, external_port, internal_port):
+        """
+        删除端口映射
+        """
+        return self._add_del(name=name,
+                             internal_ip=internal_ip,
+                             external_port=external_port,
+                             internal_port=internal_port,
+                             mode=2)
+
+    def clean(self):
+        """
+        清除全部端口映射
+        """
+        response = self.req.post(f'{ROUTE_URL}/app/portmap/webs/virtual_service_clean.cgi')
+        response.raise_for_status()
+
+        return response.json()
+
+
 class Qihoo(Router):
     """360路由"""
 
@@ -69,7 +193,11 @@ class Qihoo(Router):
         self.password = password
 
         self.req = httpx.Client(headers={'Referer': 'default'})
+
         self.blacklist = BlackList(self)
+        self.speedlimit = SpeedLimit(self)
+        self.devices = Devices(self)
+        self.virtualservice = VirtualService(self)
 
         self._login()
 
@@ -112,8 +240,3 @@ class Qihoo(Router):
             'Token-ID': response.json()['Token-ID'],
         }
         self.req.cookies.update(cookies)
-
-
-if __name__ == '__main__':
-    qh = Qihoo(USER.username, USER.password)
-    print(qh.blacklist.list())
